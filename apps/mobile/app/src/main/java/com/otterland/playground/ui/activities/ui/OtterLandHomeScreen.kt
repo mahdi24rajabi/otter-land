9f9aece42c286d042370784dd16514fb93f48e1d
mahdirajabi-nent <54574624+mahdirajabi-nent@users.noreply.github.com>
2025-11-09 15:08:34 +0100

    improve main layout and system layout

--- apps/mobile/app/src/main/java/com/otterland/playground/ui/activities/ui/OtterLandHomeScreen.kt
+++ apps/mobile/app/src/main/java/com/otterland/playground/ui/activities/ui/OtterLandHomeScreen.kt
@@ -1,13 +1,21 @@
 package com.otterland.playground.ui.activities.ui
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.safeGesturesPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationItemColors
 import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.NavigationRailItemColors
 import androidx.compose.material3.Text
+import androidx.compose.material3.WideNavigationRailDefaults
 import androidx.compose.material3.adaptive.WindowAdaptiveInfo
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteDefaults
@@ -21,6 +29,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.otterland.foundation.design.theme.OtterLandTheme
@@ -80,8 +89,14 @@ fun OtterLandHomeScreen(
         shortNavigationBarContentColor = MaterialTheme.colorScheme.surfaceContainer,
         navigationBarContainerColor = MaterialTheme.colorScheme.surfaceContainer,
         navigationBarContentColor = MaterialTheme.colorScheme.surfaceContainer,
-        navigationRailContainerColor = MaterialTheme.colorScheme.onSurface,
+        navigationRailContainerColor = MaterialTheme.colorScheme.errorContainer,
         navigationRailContentColor = MaterialTheme.colorScheme.onSurface,
+        wideNavigationRailColors = WideNavigationRailDefaults.colors(
+            contentColor = MaterialTheme.colorScheme.surfaceContainer,
+            containerColor = MaterialTheme.colorScheme.surfaceContainer,
+        ),
+        navigationDrawerContentColor = MaterialTheme.colorScheme.surfaceContainer,
+        navigationDrawerContainerColor = MaterialTheme.colorScheme.surfaceContainer,
     )
 
     val navigationBarColors = NavigationItemColors(
@@ -147,14 +162,20 @@ fun OtterLandHomeScreen(
         navigationSuiteColors = navigationSuiteColor,
         navigationItemVerticalArrangement = NavigationSuiteDefaults.verticalArrangement,
         primaryActionContent = {},
-        modifier = Modifier.safeContentPadding(),
+        modifier = Modifier.safeDrawingPadding(),
     ) {
-        when (currentNavigationItem) {
-            NavigationItems.SYSTEM_INFO -> {
-                SystemInfoScreen()
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .background(color = MaterialTheme.colorScheme.secondaryContainer)
+                .verticalScroll(rememberScrollState()),
+        ){
+            when (currentNavigationItem) {
+                NavigationItems.SYSTEM_INFO -> {
+                    SystemInfoScreen()
+                }
+                NavigationItems.FIREBASE -> {}
+                NavigationItems.MEDIA -> {}
             }
-            NavigationItems.FIREBASE -> {}
-            NavigationItems.MEDIA -> {}
         }
     }
 }
--- feature/systeminfo/src/main/java/com/outterland/feature/systeminfo/ui/SystemInfoScreen.kt
+++ feature/systeminfo/src/main/java/com/outterland/feature/systeminfo/ui/SystemInfoScreen.kt
@@ -3,9 +3,7 @@ package com.outterland.feature.systeminfo.ui
 import android.content.res.Configuration
 import android.os.Build
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,14 +12,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -30,6 +27,7 @@ import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -59,104 +57,173 @@ enum class Category(
 @OptIn(ExperimentalMaterial3AdaptiveApi::class)
 @Composable
 fun SystemInfoScreen() {
-    val cardColors = CardDefaults.elevatedCardColors(containerColor = MaterialTheme.colorScheme.surfaceContainerLow)
-    val carElevation = CardDefaults.elevatedCardElevation(defaultElevation = 1.dp)
+    val cardColors =
+        CardDefaults.elevatedCardColors(containerColor = MaterialTheme.colorScheme.surfaceContainerLow)
+    val cardElevation = CardDefaults.elevatedCardElevation(defaultElevation = 1.dp)
     val cardShape = CardDefaults.elevatedShape
 
-    Row(
-        modifier = Modifier.fillMaxSize()
+    Column(
+        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
     ) {
-        Card(
-            modifier = Modifier.fillMaxWidth()
-                .height(160.dp),
-            colors = cardColors,
-            elevation = carElevation,
-            shape = cardShape
-        ) {
-            Row {
-                HardwareCard()
-            }
-        }
+        HardwareCard(
+            cardColors = cardColors,
+            carElevation = cardElevation,
+            cardShape = cardShape,
+        )
+        HorizontalDivider(thickness = 8.dp, color = Color.Transparent)
+        HardwareCard(
+            cardColors = cardColors,
+            carElevation = cardElevation,
+            cardShape = cardShape,
+        )
+        HorizontalDivider(thickness = 8.dp, color = Color.Transparent)
+        HardwareCard(
+            cardColors = cardColors,
+            carElevation = cardElevation,
+            cardShape = cardShape,
+        )
+        HorizontalDivider(thickness = 8.dp, color = Color.Transparent)
+        HardwareCard(
+            cardColors = cardColors,
+            carElevation = cardElevation,
+            cardShape = cardShape,
+        )
+        HorizontalDivider(thickness = 8.dp, color = Color.Transparent)
+        HardwareCard(
+            cardColors = cardColors,
+            carElevation = cardElevation,
+            cardShape = cardShape,
+        )
+        HorizontalDivider(thickness = 8.dp, color = Color.Transparent)
+        HardwareCard(
+            cardColors = cardColors,
+            carElevation = cardElevation,
+            cardShape = cardShape,
+        )
+        HorizontalDivider(thickness = 8.dp, color = Color.Transparent)
+        HardwareCard(
+            cardColors = cardColors,
+            carElevation = cardElevation,
+            cardShape = cardShape,
+        )
+        HorizontalDivider(thickness = 8.dp, color = Color.Transparent)
+        HardwareCard(
+            cardColors = cardColors,
+            carElevation = cardElevation,
+            cardShape = cardShape,
+        )
     }
 }
 
 @Composable
-internal fun HardwareCard(){
-    Image(
-        painter = painterResource(R.drawable.image_processor),
-        contentDescription = "",
-        modifier = Modifier.fillMaxHeight()
-            .fillMaxWidth(0.5f)
-            .graphicsLayer(alpha = 0.99f)
-            .drawWithContent {
-                drawContent()
-                drawRect(
-                    brush = Brush.horizontalGradient(
-                        colors = listOf(Color.Black, Color.Transparent),
-                        startX = size.width * 0.50f,
-                        endX = size.width
-                    ),
-                    blendMode = BlendMode.DstIn
-                )
-            },
-        contentScale = ContentScale.FillHeight,
-        alignment = Alignment.CenterStart,
-    )
-
-    Column(
-        modifier = Modifier.fillMaxSize().padding(
-            top = 16.dp,
-            bottom = 16.dp,
-            end = 16.dp
-        )
+internal fun HardwareCard(
+    cardColors: CardColors,
+    carElevation: CardElevation,
+    cardShape: Shape,
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(160.dp),
+        colors = cardColors,
+        elevation = carElevation,
+        shape = cardShape
     ) {
-        Text(
-            text = "Hardware",
-            color = MaterialTheme.typography.titleMedium.color,
-            fontFamily = MaterialTheme.typography.titleMedium.fontFamily,
-            modifier = Modifier.wrapContentSize(),
-            fontSize = MaterialTheme.typography.titleMedium.fontSize
-        )
-        HorizontalDivider(
-            color = Color.Transparent,
-            thickness = 8.dp
-        )
         Row(
-            modifier = Modifier.wrapContentHeight().fillMaxWidth()
+            modifier = Modifier.fillMaxSize()
         ) {
-            Text(
-                text = "Type:",
-                color = MaterialTheme.typography.bodySmall.color,
-                fontFamily = MaterialTheme.typography.bodySmall.fontFamily,
-                modifier = Modifier.wrapContentHeight().fillMaxWidth(0.5f),
-                fontSize = MaterialTheme.typography.bodySmall.fontSize
+            Image(
+                painter = painterResource(R.drawable.image_processor),
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(0.5f)
+                    .graphicsLayer(alpha = 0.99f)
+                    .drawWithContent {
+                        drawContent()
+                        drawRect(
+                            brush = Brush.horizontalGradient(
+                                colors = listOf(Color.Black, Color.Transparent),
+                                startX = size.width * 0.50f,
+                                endX = size.width
+                            ),
+                            blendMode = BlendMode.DstIn
+                        )
+                    },
+                contentScale = ContentScale.FillHeight,
+                alignment = Alignment.CenterStart,
             )
-            Text(
-                text = Build.HARDWARE,
-                color = MaterialTheme.typography.bodySmall.color,
-                fontFamily = MaterialTheme.typography.bodySmall.fontFamily,
-                modifier = Modifier.wrapContentHeight().fillMaxWidth(),
-                fontSize = MaterialTheme.typography.bodySmall.fontSize
-            )
-        }
 
-        Row(
-            modifier = Modifier.wrapContentHeight().fillMaxWidth()
-        ) {
-            Text(
-                text = "Core:",
-                color = MaterialTheme.typography.bodySmall.color,
-                fontFamily = MaterialTheme.typography.bodySmall.fontFamily,
-                modifier = Modifier.wrapContentHeight().fillMaxWidth(0.5f),
-                fontSize = MaterialTheme.typography.bodySmall.fontSize
-            )
-            Text(
-                text = "4",
-                color = MaterialTheme.typography.bodySmall.color,
-                fontFamily = MaterialTheme.typography.bodySmall.fontFamily,
-                modifier = Modifier.wrapContentHeight().fillMaxWidth(),
-                fontSize = MaterialTheme.typography.bodySmall.fontSize
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        top = 16.dp,
+                        bottom = 16.dp,
+                        end = 16.dp
+                    )
+            ) {
+                Text(
+                    text = "Hardware",
+                    color = MaterialTheme.typography.titleMedium.color,
+                    fontFamily = MaterialTheme.typography.titleMedium.fontFamily,
+                    modifier = Modifier.wrapContentSize(),
+                    fontSize = MaterialTheme.typography.titleMedium.fontSize
+                )
+                HorizontalDivider(
+                    color = Color.Transparent,
+                    thickness = 8.dp
+                )
+                Row(
+                    modifier = Modifier
+                        .wrapContentHeight()
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Type:",
+                        color = MaterialTheme.typography.bodySmall.color,
+                        fontFamily = MaterialTheme.typography.bodySmall.fontFamily,
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxWidth(0.5f),
+                        fontSize = MaterialTheme.typography.bodySmall.fontSize
+                    )
+                    Text(
+                        text = Build.HARDWARE,
+                        color = MaterialTheme.typography.bodySmall.color,
+                        fontFamily = MaterialTheme.typography.bodySmall.fontFamily,
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxWidth(),
+                        fontSize = MaterialTheme.typography.bodySmall.fontSize
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .wrapContentHeight()
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Core:",
+                        color = MaterialTheme.typography.bodySmall.color,
+                        fontFamily = MaterialTheme.typography.bodySmall.fontFamily,
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxWidth(0.5f),
+                        fontSize = MaterialTheme.typography.bodySmall.fontSize
+                    )
+                    Text(
+                        text = "4",
+                        color = MaterialTheme.typography.bodySmall.color,
+                        fontFamily = MaterialTheme.typography.bodySmall.fontFamily,
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxWidth(),
+                        fontSize = MaterialTheme.typography.bodySmall.fontSize
+                    )
+                }
+            }
         }
     }
 }
